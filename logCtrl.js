c4539fcfb33b953daf5f856d42db84793239f5a1
Lukasz Koziol <lukasz.koziol@emc.com>
2016-07-06 10:23:24 +0200

    add date filter, update docs

--- logCtrl.js
+++ logCtrl.js
@@ -1,16 +1,28 @@
 var request = require('request');
+var dateFormat = require('dateformat');
 var app = angular.module('kewl', []);
 
 app.controller('logCtrl', function ($scope, $http) {
 
-    $scope.appNames = [dev-prom-gateway", "dev-titan-notification-management",
+    $scope.appNames = ["", "dev-prom-gateway", "dev-titan-notification-management",
                        "dev-prom-entity-definition", "dev-vnx-asset-management"];
-    $scope.levels = ["INFO", "DEBUG", "WARN", "ERROR", "FATAL"];
+    $scope.levels = ["", "INFO", "DEBUG", "WARN", "ERROR", "FATAL"];
+    $scope.selectedAppName = "";
+    $scope.selectedLevel = "";
+    $scope.selectedDate = null;
 
-    var elasticSearchBaseUrl = 'http://10.73.66.63:9200/_all/_search';
+    var elasticSearchBaseUrl = 'http://10.73.66.63:9200/';
+    var searchAll = '_all/';
+
     function updateLogs() {
 
         var url = elasticSearchBaseUrl;
+        if($scope.selectedDate) {
+            url += "logstash-" + dateFormat($scope.selectedDate, "yyyy.mm.dd") + '/syslog/'
+        } else {
+            url += searchAll;
+        }
+        url += '_search';
 
         var params = "";
         if ($scope.selectedAppName)
@@ -34,6 +46,9 @@ app.controller('logCtrl', function ($scope, $http) {
         $http.get(url)
             .then(function (response) {
                 $scope.logs = response.data.hits.hits;
+            },
+            function (response) {
+                $scope.logs = [];
             });
     }
 
